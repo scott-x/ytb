9662d0b56f78381c8c0b634abf99b63ff74c1012
scott-x <mbp98k@gmail.com>
2020-11-28 12:19:13 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # ytb
+
+This tool is based on [youtube-dl](https://github.com/ytdl-org/youtube-dl), to use it please make sure golang was installed & Youtube can be visited.
+
+
+### how to use?
+
+1, downloading ytb tool
+
+```bash
+go install github.com/scott-x/ytb
+```
+
+2, configuration
+
+```bash
+mkdir ~/.ytb
+touc ~/.ytb/youtube-dl.json
+```
+
+`/youtube-dl.json` has 2 properties as shown below:
+
+```json
+{
+	"to":"~/Desktop/%(title)s.%(ext)s",
+	"task_position":"/Users/scottxiong/Desktop/task.txt"
+}
+```
+
+- `to`: `to` indicates where these video will go to 
+- `task_position`: before downloading, list the download youtube url line by line.
+
+
+For Example:
+
+![](./task.png)
+
+```bash
+$ go run main.go
+set http/https proxy...
+start downloading ===> https://www.youtube.com/watch?v=euf7BCpWZgA
+start downloading ===> https://www.youtube.com/watch?v=BkPMZDgfH4M
+start downloading ===> https://www.youtube.com/watch?v=qXlS-X0ddsA
+https://www.youtube.com/watch?v=euf7BCpWZgA ===> 100% downloaded
+2020/11/28 12:06:26 https://www.youtube.com/watch?v=euf7BCpWZgA is deleted from /Users/scottxiong/Desktop/task.txt
+https://www.youtube.com/watch?v=qXlS-X0ddsA ===> 100% downloaded
+2020/11/28 12:06:54 https://www.youtube.com/watch?v=qXlS-X0ddsA is deleted from /Users/scottxiong/Desktop/task.txt
+https://www.youtube.com/watch?v=BkPMZDgfH4M ===> 100% downloaded
+2020/11/28 12:12:31 https://www.youtube.com/watch?v=BkPMZDgfH4M is deleted from /Users/scottxiong/Desktop/task.txt
+```
--- main.go
+++ main.go
@@ -7,17 +7,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"sync"
 )
 
 var (
 	where string
 	to    string
+	wg    = sync.WaitGroup{}
 )
+
+type YTB struct {
+	To            string `json:"to"`
+	Task_position string `json:"task_position"`
+}
 
 func init() {
 	HOME, flag := os.LookupEnv("HOME")
@@ -25,7 +34,7 @@ func init() {
 		log.Println("$HOME should be set")
 		return
 	}
-	where = HOME + "/.ytb/youtube-dl.conf"
+	where = HOME + "/.ytb/youtube-dl.json"
 }
 
 func main() {
@@ -33,35 +42,75 @@ func main() {
 		log.Println("configuration file " + where + " not found, could you please check")
 		return
 	}
+	to, task_position := getConfig(where)
 
-	bs, err := ioutil.ReadFile(where)
+	if len(to) == 0 {
+		to = "~/Desktop/%(title)s.%(ext)s"
+	}
+
+	if task_position == "" {
+		log.Println("The field task_position should not be null")
+		return
+	}
+
+	//get urls
+	content, err := ioutil.ReadFile(task_position)
 	if err != nil {
 		panic(err)
 	}
 
-	if len(bs) == 0 {
-		to = "~/Desktop/%(title)s.%(ext)s"
-	} else {
-		to = string(bs)
+	if strings.TrimSpace(string(content)) == "" {
+		log.Println("No task is set in " + task_position)
+		return
 	}
+	con := strings.Trim(string(content), "\n")
+	urls := strings.Split(con, "\n")
 
 	//set proxy
 	setProxy()
-	download(to, "https://www.youtube.com/watch?v=C6FhEonS-SU")
 
-}
-
-func downloadToWhere() string {
-	return ""
-}
-
-func download(to, url string) {
-	log.Println("start downloading " + url)
-	cmd := exec.Command("youtube-dl", "-o", to, url)
-	err := cmd.Start()
-	if err != nil {
-		log.Println("download " + url + " error")
+	for _, url := range urls {
+		wg.Add(1)
+		// download(to, "https://www.youtube.com/watch?v=C6FhEonS-SU")
+		go download(to, url, task_position)
 	}
+
+	wg.Wait()
+
+}
+
+func deleteFinishedUrl(task_position, url string) {
+	bs, err := ioutil.ReadFile(task_position)
+	if err != nil {
+		panic(err)
+	}
+	content := strings.ReplaceAll(string(bs), url, "")
+	err = ioutil.WriteFile(task_position, []byte(content), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(url) > 0 {
+		log.Println(url + " is deleted from " + task_position)
+	}
+}
+
+func download(to, url, task_position string) {
+	defer wg.Done()
+
+	if len(url) > 0 {
+		fmt.Println("start downloading ===> " + url)
+	}
+
+	cmd := exec.Command("youtube-dl", "--include-ads", "-i", "-c", "-o", to, url)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("download %s error:%s\n", url, err)
+	}
+	if len(url) > 0 {
+		fmt.Println(url + ` ===> 100% downloaded`)
+	}
+
+	deleteFinishedUrl(task_position, url)
 }
 
 func isFileExist(name string) bool {
@@ -74,5 +123,27 @@ func isFileExist(name string) bool {
 }
 
 func setProxy() {
-	fmt.Println("proxy")
+	fmt.Println("set http/https proxy...")
+	err := os.Setenv("http_proxy", "http://127.0.0.1:1024")
+	if err != nil {
+		log.Printf("set $http_proxy error:%s", err)
+	}
+	err = os.Setenv("https_proxy", "http://127.0.0.1:1024")
+	if err != nil {
+		log.Printf("set $https_proxy error:%s", err)
+	}
+}
+
+func getConfig(where string) (string, string) {
+	bs, err := ioutil.ReadFile(where)
+	if err != nil {
+		panic(err)
+	}
+
+	ytb := &YTB{}
+	err = json.Unmarshal(bs, ytb)
+	if err != nil {
+		panic(err)
+	}
+	return ytb.To, ytb.Task_position
 }
